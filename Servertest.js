835b9b73312843feb0626433fb65f9efff418204
Astha <yastha.as@gmail.com>
2018-11-13 21:55:17 +0545

    Server test through local host

---
+++ Servertest.js
@@ -0,0 +1,10 @@
+const express = require ('express');
+const app = express();
+const port = 3000;
+app.get('/', function(request, response){
+response.send('Hello, World');
+});
+
+app.listen(port, function(){
+console.log('Server listening on http://localhost' + port);
+});
